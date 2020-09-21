8cfb63ef08d4feb5221539cfc73664cc48701670
Zachary Meadows <zmeadows@gmail.com>
2020-09-21 11:54:04 -0700

    added --source command line option

--- src/main.cpp
+++ src/main.cpp
@@ -24,12 +24,13 @@ int main(int argc, char** argv)
 
     // TODO: make mutually exclusive arguments, for example attach vs. launch file
     // clang-format off
-    options.allow_unrecognised_options().add_options()
+    options.add_options()
         ("n,attach-name", "Tells the debugger to attach to a process with the given name.", cxxopts::value<std::string>())
         ("p,attach-pid", "Tells the debugger to attach to a process with the given pid.", cxxopts::value<std::string>())
         ("w,wait-for", "Tells the debugger to wait for a process with the given pid or name to launch before attaching.", cxxopts::value<std::string>())
         ("f,file", "Tells the debugger to use <filename> as the program to be debugged.", cxxopts::value<std::string>())
         ("S,source-before-file", "Tells the debugger to read in and execute the lldb  commands  in the given file, before any file has been loaded.", cxxopts::value<std::string>())
+        ("s,source", "Tells the debugger to read in and execute the lldb commands in the given file, after any file has been loaded.", cxxopts::value<std::string>())
         ("workdir", "Specify base directory of file explorer tree", cxxopts::value<std::string>())
         ("h,help", "Print out usage information.")
         ("positional", "Positional arguments: these are the arguments that are entered without an option", cxxopts::value<std::vector<std::string>>())
@@ -69,19 +70,14 @@ int main(int argc, char** argv)
 
     Application app(*ui, workdir);
 
-    // TODO: test this
     if (result.count("source-before-file")) {
         const std::string source_path = result["source-before-file"].as<std::string>();
         auto handle = FileHandle::create(source_path);
         if (handle.has_value()) {
             for (const std::string& line : handle->contents()) {
                 auto ret = run_lldb_command(app, line.c_str());
-                if (!ret.IsValid()) {
-                    LOG(Error) << "Error parsing source file line: " << line;
-                    break;
-                }
             }
-            LOG(Debug) << "Successfully executed commands in source file: " << source_path;
+            LOG(Verbose) << "Successfully executed commands in source file: " << source_path;
         }
         else {
             LOG(Error) << "Invalid filepath passed to source-before-file argument: " << source_path;
@@ -105,7 +101,19 @@ int main(int argc, char** argv)
         }
     }
 
-    run_lldb_command(app, "breakpoint set --file test.cpp --line 24");
+    if (result.count("source")) {
+        const std::string source_path = result["source"].as<std::string>();
+        auto handle = FileHandle::create(source_path);
+        if (handle.has_value()) {
+            for (const std::string& line : handle->contents()) {
+                auto ret = run_lldb_command(app, line.c_str());
+            }
+            LOG(Verbose) << "Successfully executed commands in source file: " << source_path;
+        }
+        else {
+            LOG(Error) << "Invalid filepath passed to --source argument: " << source_path;
+        }
+    }
 
     return main_loop(app);
 }
