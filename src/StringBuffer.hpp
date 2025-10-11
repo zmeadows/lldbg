28972d8bb141d18798efcf202c24910405183167
Zachary Meadows <zmeadows@gmail.com>
2025-10-11 16:50:45 -0700

    replace call to deprecated fmt::appender function

--- src/StringBuffer.hpp
+++ src/StringBuffer.hpp
@@ -5,29 +5,28 @@
 
 // Just a convenience class to ensure all fmt buffers are nul-terminated
 class StringBuffer {
-    fmt::basic_memory_buffer<char> m_buffer;
+  fmt::basic_memory_buffer<char> m_buffer;
 
 public:
-    template <typename... Args>
-    inline void format(const char* fmt_str, Args&&... args)
-    {
-        // TODO: loop over args parameters, check if they are pointers and if they are null, don't
-        // call fmt::format_to
-        fmt::format_to(fmt::appender(m_buffer), fmt_str, args...);
-        m_buffer.push_back('\0');
-    }
+  template <typename... Args> void format(const char *fmt_str, Args &&...args) {
+    // TODO: loop over args parameters, check if they are pointers and if they
+    // are null, don't call fmt::format_to
+    fmt::format_to(std::back_inserter(m_buffer), fmt_str,
+                   std::forward<Args>(args)...);
+    m_buffer.push_back('\0');
+  }
 
-    template <typename... Args>
-    inline void format_(const char* fmt_str, Args&&... args)
-    {
-        // TODO: loop over args parameters, check if they are pointers and if they are null, don't
-        // call fmt::format_to
-        fmt::format_to(fmt::appender(m_buffer), fmt_str, args...);
-    }
+  template <typename... Args>
+  void format_(const char *fmt_str, Args &&...args) {
+    // TODO: loop over args parameters, check if they are pointers and if they
+    // are null, don't call fmt::format_to
+    fmt::format_to(std::back_inserter(m_buffer), fmt_str,
+                   std::forward<Args>(args)...);
+  }
 
-    inline const char* data(void) { return m_buffer.data(); }
+  const char *data() { return m_buffer.data(); }
 
-    inline void clear(void) { m_buffer.clear(); }
+  void clear() { m_buffer.clear(); }
 
-    inline void push_back(char c) { m_buffer.push_back(c); }
+  void push_back(char chr) { m_buffer.push_back(chr); }
 };
